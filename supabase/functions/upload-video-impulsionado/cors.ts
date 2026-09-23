5a406c1d6ec1633e6d2358f14844549437c7660e
brunazayanne <bruna.vieira@adventuresbrands.com>
2026-09-23 20:06:36 -0300

    Create cors.ts

---
+++ supabase/functions/upload-video-impulsionado/cors.ts
@@ -0,0 +1,6 @@
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type, content-range, x-drive-upload-url",
+  "Access-Control-Allow-Methods": "POST, PUT, OPTIONS",
+};
